0c648aa84e746770e4143734a6e3256d9269b050
Cyboulette <cyboulette58@gmail.com>
2021-02-27 14:00:41 +0100

    FIX-6: handle Sqlite & Postgres schema migration and fix typo on Mysql schema

--- Schema/Mysql.php
+++ Schema/Mysql.php
@@ -13,7 +13,7 @@ function version_1(PDO $pdo)
             group_id INT NOT NULL,
             user_id INT NOT NULL,
             FOREIGN KEY(group_id) REFERENCES `groups`(id) ON DELETE CASCADE,
-            FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+            FOREIGN KEY(user_id) REFERENCES `users`(id) ON DELETE CASCADE,
             UNIQUE(group_id, user_id)
         ) ENGINE=InnoDB CHARSET=utf8
     ");
