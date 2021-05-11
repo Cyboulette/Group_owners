c1c19e4b0196bf4d13dedfea2463f7b1f698d714
Luis <luisfelipeandrade@gmail.com>
2021-05-10 23:13:46 -0300

    Add portuguese brazilian translate

---
+++ Locale/pt_BR/translations.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+    'Allows users to create and manage their groups by themselves' => 'Permite que os usuários criem e gerenciem seus grupos por conta própria',
+    'Proprietário do grupo' => 'Proprietário do grupo',
+    'Group Member' => 'Membro do grupo',
+    'You are not member or not owner of any groups.' => 'Você não é membro ou não é proprietário de nenhum grupo',
+    'No groups are corresponding to your research.' => 'Nenhum grupo corresponde à sua pesquisa.',
+    'Manage my groups' => 'Gerenciar meus grupos',
+    'Add me as member of this group' => 'Me adicione como membro deste grupo',
+    'You are not owner of this group.' => 'Você não é o proprietário deste grupo.',
+    'will be added as member of this group.' => 'será adicionado como membro deste grupo.',
+    'will be added as owner of this group.' => 'será adicionado como proprietário deste grupo.',
+    'Add the role "group member" for this user' => 'Adicione a função "membro do grupo" para este usuário',
+    'Add the role "group owner" for this user' => 'Adicione a função "proprietário do grupo" para este usuário',
+    'Remove "group owner" role for this user' => 'Remova a função de "proprietário do grupo" para este usuário',
+    'Remove "group member" role for this user' => 'Remova a função de "membro do grupo" para este usuário',
+    'Do you really want to remove the "group owner" role for the user "%s" from the group "%s"?' => 'Deseja realmente remover a função de "proprietário do grupo" para o usuário "%s" do grupo "%s"?',
+    '"Group owner" role for this user removed successfully from this group.' => 'A função "Proprietário do grupo" para este usuário foi removida com sucesso deste grupo.',
+    'Unable to remove the "group owner" role for this user from the group.' => 'Não foi possível remover a função de "proprietário do grupo" para este usuário do grupo.',
+    'Unable to add group owner.' => 'Não é possível adicionar o proprietário do grupo',
+    '"Group owner" role added successfully.' => 'Função de "proprietário do grupo" adicionada com sucesso',
+    'Add group owner' => 'Adicionar proprietário de grupo',
+    'Display icon on application dropdown' => 'Exibir ícone na lista suspensa do aplicativo',
+    'GroupOwners plugin settings' => 'Configurações do plugin GrupOwners'
+);
